50cb20214786d28b2047828baf12be9c610111cb
Wiktoria Pietrzyk <pietrzykwiktoria01@gmail.com>
2023-05-06 22:00:30 +0200

    changes

--- src/pages/home-page/scripts/home-page.js
+++ src/pages/home-page/scripts/home-page.js
@@ -1,7 +1,4 @@
 /// Homepage JavaScript File
-
-import '../styles/home-page.scss'
-
 // This is the function, which replaces classes of the elemets and change the appearance of the button element.
 
 let button = document.getElementById('btn');
